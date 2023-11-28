f66200bcbd1cb8cfd8905c14d777ffc7a0659ca7
Professor José de Assis <josedeassisfilho@gmail.com>
2023-11-28 15:54:19 -0300

    Update setar_HC-06.ino

--- setar_HC-06/setar_HC-06.ino
+++ setar_HC-06/setar_HC-06.ino
@@ -3,13 +3,13 @@
   @author Professor José de Assis
   @link https://roboticapratica.com.br/
   
-   ------------------
+  --------------------
   | Módulo | Arduino |
   | VCC    | 5V      |
   | GND    | GND     |
   | TX     | pino 2  |
   | RX *   | pino 3  |
-   ------------------
+  --------------------
 
    * Usar divisor de tensão
 
@@ -19,37 +19,41 @@
    \
      ----- 2K ----- GND   
 
+   * Configure o Serial Monitor para [No Line Ending] e [115200 baud]
+
 */
 
 #include <SoftwareSerial.h>
 SoftwareSerial modulo(2, 3);
 String comando = "";
 void setup() {
-  modulo.begin(9600);
+  modulo.begin(9600);  
   Serial.begin(115200);
-  Serial.println("1. Configure o monitor serial para [No Line Ending] e [115200 baud]");
+  Serial.println("1. Configure o Serial Monitor para [No Line Ending] e [115200 baud]");
   Serial.println("2. Digite o comando desejado na caixa de entrada");
-  Serial.println(" ----------------------------------------------");
-  Serial.println("| COMANDO      | RESPOSTA                      |");
-  Serial.println("| AT           | OK                            |");
-  Serial.println("| AT+VERSION   | (identificar firmware)        |");
-  Serial.println("| AT+NAME      | (mudar SSID) Ex: AT+NAMEHC06  |");
-  Serial.println("| AT+PIN       | (mudar senha) Ex: AT+PIN1234  |");
-  Serial.println(" ----------------------------------------------"); 
-  Serial.println("Resposta:");                     
+  Serial.println("-----------------------------------------------------------");
+  Serial.println("| Comando    | Finalidade                   | Resposta    |");
+  Serial.println("-----------------------------------------------------------");
+  Serial.println("| AT         | Teste                        | OK          |");
+  Serial.println("| AT+VERSION | Identificar firmware         | OKlinvorVer |");
+  Serial.println("| AT+NAME    | Mudar SSID Ex: AT+NAMEHC-06  | OKsetname   |");
+  Serial.println("| AT+PIN     | Mudar senha Ex: AT+PIN1234   | OKsetPIN    |");
+  Serial.println("|            |(Senha numerica de 4 digitos) |             |");
+  Serial.println("----------------------------------------------------------");
+  Serial.println("Resposta:");
 }
 
 void loop() {
-  if (modulo.available())  {
-    while (modulo.available())    {
+  if (modulo.available()) {
+    while (modulo.available()) {
       comando += (char)modulo.read();
     }
     Serial.print(comando);
-    comando = "";    
+    comando = "";
   }
 
-  if (Serial.available())  {
+  if (Serial.available()) {
     delay(10);
-    modulo.write(Serial.read());   
+    modulo.write(Serial.read());
   }
 }
